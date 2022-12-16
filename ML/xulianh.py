313629d3e13a4a54f18cbc9a176b2c42c1be6625
NGONGOCHA <90134905+NGONGOCHA@users.noreply.github.com>
2022-12-17 02:50:09 +0700

    Add files via upload

--- ML/xulianh.py
+++ ML/xulianh.py
@@ -7,11 +7,12 @@ from PIL import Image
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 
+#HÀM KIỂM TRA ẢNH CÓ LỖI HAY KHÔNG
 def check_corrupted_image(img_file):
     try:
         with Image.open(img_file) as img:
             img.verify()
-            img_new = io.imread(os.path.join(img_file))
+            io.imread(os.path.join(img_file))
         return False
     except Exception as e:
         print(e)
@@ -31,14 +32,30 @@ def read_img_data(path,label, size):
     X = np.array(X)
     return X,y
 
-def main():
-    X,y = read_img_data('C:/Users/Ngoc Ha/Desktop/hoc may/PetImages/Cat','cat', (32,32))
-    X_dog, y_dog = read_img_data('C:/Users/Ngoc Ha/Desktop/hoc may/PetImages/Dog','dog', (32,32))
-    y.extend(y_dog)
-    X = np.array(X)
-    y = LabelBinarizer().fit_transform(y)
-    print("x shape", X.shape)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=True, random_state =1)
+#hàm chuyển ảnh màn hình thành vector 1024
+def convert_D_2_vector(path,label,size):
+    labels = []
+    img_data = []
+    images = os.listdir(path)
+    for img_file in images:
+        if not(check_corrupted_image(os.path.join(path,img_file))):
+            img_grey = io.imread(os.path.join(path,img_file), as_grey = True)
+            img_vector = resize(img_grey,size).flatten()
+            img_data.append(label)
+    return img_data, labels
 
-if __name__ == '__main__':
-    main()
+#hàm xây dựng cơ sở dữ liệu ảnh mô hình
+def build_img_data():
+    cat_path = read_img_data('C:/Users/Ngoc Ha/Desktop/hoc may/PetImages/Cat', 'Cat', (32,32))
+    dog_path = read_img_data('C:/Users/Ngoc Ha/Desktop/hoc may/PetImages/Dog', 'Dog', (32,32))
+    images,labels = convert_D_2_vector(cat_path, 'Cat', (32,32))
+    img_dogs, label_dogs = convert_D_2_vector(dog_path, 'Dog', (32,32))
+    images.extend(img_dogs)
+    labels.extend(label_dogs)
+    X = np.array(images)
+    y = LabelBinarizer().fit_transform(labels)
+    return X,y
+
+def train_test(D_dog, D_cat):
+    D_dog, D_cat = D[:, :-1], D[:, -1]
+    X_train, X_test, y_train, y_t
